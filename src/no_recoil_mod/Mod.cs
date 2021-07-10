693d88ffa24f8bd1cc230e045842dbb31cc027d9
EEX-bsg <exendra314@gmail.com>
2021-07-10 17:19:09 +0900

    firstcommit

---
+++ src/no_recoil_mod/Mod.cs
@@ -0,0 +1,14 @@
+using System;
+using Modding;
+using UnityEngine;
+
+namespace no_recoil_mod
+{
+	public class Mod : ModEntryPoint
+	{
+		public override void OnLoad()
+		{
+			// Called when the mod is loaded.
+		}
+	}
+}
