b6e327e8ef582f0a2234989b12b66df8028b9b01
EEX-bsg <exendra314@gmail.com>
2021-07-10 19:24:20 +0900

    ログ関数追加　バージョン表示追加(NotRecoilModule)

--- src/no_recoil_mod/Mod.cs
+++ src/no_recoil_mod/Mod.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using Modding;
 using UnityEngine;
 
@@ -6,9 +7,45 @@ namespace no_recoil_mod
 {
 	public class Mod : ModEntryPoint
 	{
-		public override void OnLoad()
+        public static void Log(string message)
+        {
+            ModConsole.Log("NoRecoil Log:" + message);
+        }
+        public static void Warning(string message)
+        {
+            ModConsole.Log("NoRecoil Warning:" + message);
+        }
+        public static void Error(string message)
+        {
+            ModConsole.Log("NoRecoil Error:" + message);
+        }
+        public override void OnLoad()
 		{
-			// Called when the mod is loaded.
-		}
-	}
+            // Called when the mod is loaded.
+            UnityEngine.Object.DontDestroyOnLoad(SingleInstance<NotRecoilModule>.Instance);
+
+        }
+        public class NotRecoilModule : SingleInstance<NotRecoilModule>
+        {
+            public override string Name
+            {
+                get
+                {
+                    return "ETCMmodule";
+
+                }
+            }
+            private IEnumerator CheckVersion()
+            {
+                //âΩÇ∆Ç»Ç≠1ïbë“ã@(èàóùèáóp)
+                yield return new WaitForSeconds(1f);
+                //GuidÇ…ÇÕÇÕé©ï™ÇÃMod.xmlì‡ÇÃIDÇì¸ÇÍÇÈÇ±Ç∆
+                Log("Version " + Mods.GetVersion(new Guid("ed3ed2d0-a918-45e2-828e-6cc08df2784a")));
+            }
+            public void Awake()
+            {
+                StartCoroutine(CheckVersion());
+            }
+        }
+    }
 }
